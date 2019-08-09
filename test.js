91e463ffcb0ac917bd49094a6b6ac15e273e5bb7
Ivan Kostadinov <ivank@gamehouse.com>
2019-08-09 13:58:42 +0200

    test

--- test.js
+++ test.js
@@ -5,13 +5,14 @@ const configVersionFile = './config_version.json';
 const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().replace(/\n/, '');
 
 const generateVersion = () => {
-    return new Date().toISOString().split(".")[0].replace(/\D/g,'')
+    return new Date().toISOString().split(".")[0].replace(/\D/g,'');
 };
 
 const setVersion = () => {
-    fs.writeFileSync(configVersionFile, JSON.stringify({version: generateVersion()}));
+    const version = generateVersion();
+    fs.writeFileSync(configVersionFile, JSON.stringify({version}));
+    console.log(`Version updated to ${version}`);
 };
-
 
 if (currentBranch === "master") {
     try {
